91bf23d5a74c05549a91422b757e401f7c6bbf42
yuki iwabuchi <yiwabuchi20@gmail.com>
2022-06-12 14:23:03 +0900

    add: Helloコンポーネントを作成した

--- App.jsx
+++ App.jsx
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import Hello from './src/components/Hello';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Hello bang>World</Hello>
+      <Hello bang style={{ backgroundColor: 'red', fontSize: 16 }}>
+        Small World
+      </Hello>
       <Text>Open up App.js to start working on your app!</Text>
       {/* eslint-disable */}
       <StatusBar style='auto' />
